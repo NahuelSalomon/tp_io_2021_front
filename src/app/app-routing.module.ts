9bc40e61137e3e6dd1598ae3439f597a4befb7eb
NahuelSalomon <nahuelmdp@hotmail.com>
2021-10-23 20:52:21 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductAddComponent } from './components/product/product-add/product-add.component';
+import { ProductListComponent } from './components/product/product-list/product-list.component';
+import { SupplierAddComponent } from './components/supplier/supplier-add/supplier-add.component';
+import { SupplierListComponent } from './components/supplier/supplier-list/supplier-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "product/list", component: ProductListComponent},
+  {path: "product/add", component: ProductAddComponent},
+  {path: "supplier/list", component: SupplierListComponent},
+  {path: "supplier/add", component: SupplierAddComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductAddComponent } from './components/product/product-add/product-add.component';
+import { ProductListComponent } from './components/product/product-list/product-list.component';
+import { SupplierListComponent } from './components/supplier/supplier-list/supplier-list.component';
+import { SupplierAddComponent } from './components/supplier/supplier-add/supplier-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductAddComponent,
+    ProductListComponent,
+    SupplierListComponent,
+    SupplierAddComponent
   ],
   imports: [
     BrowserModule,
