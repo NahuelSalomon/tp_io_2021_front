4d0dac7a3a756fd9966207ac63ad0c3203bc4808
noebenitez <noebenitez_03@hotmail.com>
2021-11-01 21:11:04 -0300

    Add supplier functional

--- src/app/components/supplier/supplier-add/supplier-add.component.ts
+++ src/app/components/supplier/supplier-add/supplier-add.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CustomValidator } from 'src/app/common/custom-validator';
+import { Supplier } from 'src/app/models/supplier';
+import { SupplierService } from 'src/app/services/supplier.service';
+
 
 @Component({
   selector: 'app-supplier-add',
   templateUrl: './supplier-add.component.html',
   styleUrls: ['./supplier-add.component.css']
 })
-export class SupplierAddComponent implements OnInit {
+export class SupplierAddComponent {
 
-  constructor() { }
+  constructor(private supplierService :SupplierService) { }
 
-  ngOnInit(): void {
+  supplierForm = new FormGroup({
+    name: new FormControl('', [ Validators.required]),
+    phoneNumber: new FormControl('', [ Validators.required]),
+    leadTime: new FormControl('', [ Validators.required, CustomValidator.positiveNumbersOnly()]),
+    reviewPeriod: new FormControl('', [ Validators.required, CustomValidator.positiveNumbersOnly()]),
+    isPresale: new FormControl('')
+  });
+
+  get name(){ return this.supplierForm.get('name'); }
+  get phoneNumber(){ return this.supplierForm.get('phoneNumber'); }
+  get leadTime(){ return this.supplierForm.get('leadTime'); }
+  get reviewPeriod(){ return this.supplierForm.get('reviewPeriod'); }
+  get isPresale(){ return this.supplierForm.get('isPresale'); }
+
+  
+  onSubmit(){
+
+    let name :string = this.name.value;
+    let phoneNumber :number = this.phoneNumber.value;
+    let leadTime :number = this.leadTime.value;
+    let reviewPeriod :number = this.reviewPeriod.value;
+    console.log(this.isPresale.value);
+    let isPresale :boolean = this.isPresale.value;
+
+    let supplier = new Supplier(null, name, phoneNumber, isPresale, leadTime, reviewPeriod, new Date());
+    console.log(supplier);
+    this.supplierService.add(supplier)
+      .then(response=>console.log(response))
+      .catch(error=>console.log(error))
+
   }
 
 }
